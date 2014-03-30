0246f34a32c881b202ea4e021e17a803ba31a7fd
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2014-03-30 12:43:13 -0700

    Adds interpolated triangle test case.

--- test/src/render.cpp
+++ test/src/render.cpp
@@ -255,8 +255,8 @@ TEST(RenderConstantRedWorldTriangle)
     VECTOR3 Up;
 
     PointInitialize(&TriangleVertex0, (FLOAT) 0.0, (FLOAT) 0.0, (FLOAT) 0.0);
-    PointInitialize(&TriangleVertex1, (FLOAT) 0.0, (FLOAT) 1.0, (FLOAT) 0.0);
-    PointInitialize(&TriangleVertex2, (FLOAT) 1.0, (FLOAT) 0.0, (FLOAT) 0.0);
+    PointInitialize(&TriangleVertex1, (FLOAT) 0.0, (FLOAT) 1.5, (FLOAT) 0.0);
+    PointInitialize(&TriangleVertex2, (FLOAT) 1.5, (FLOAT) 0.0, (FLOAT) 0.0);
 
     Color3InitializeFromComponents(&TriangleColor, 
                                    (FLOAT) 1.0, 
@@ -312,4 +312,103 @@ TEST(RenderConstantRedWorldTriangle)
     CHECK_EQUAL(ISTATUS_SUCCESS, Status);
 
     Success = WritePfm(Framebuffer, "RenderConstantRedWorldTriangle.pfm");
+}
+
+TEST(RenderInterpolatedRedWorldTriangle)
+{
+    PEMISSIVE_SHADER InterpolatedEmissiveShader;
+    PEMISSIVE_SHADER ConstantShader0;
+    PEMISSIVE_SHADER ConstantShader1;
+    PEMISSIVE_SHADER ConstantShader2;
+    PRAYSHADER RecursiveRayTracer;
+    PFRAMEBUFFER Framebuffer;
+    VECTOR3 CameraDirection;
+    POINT3 PinholeLocation;
+    PDRAWING_SHAPE Triangle;
+    SHADER TriangleShader;
+    POINT3 TriangleVertex0;
+    POINT3 TriangleVertex1;
+    POINT3 TriangleVertex2;
+    COLOR3 Red;
+    COLOR3 Green;
+    COLOR3 Blue;
+    ISTATUS Status;
+    PSCENE Scene;
+    bool Success;
+    VECTOR3 Up;
+
+    PointInitialize(&TriangleVertex0, (FLOAT) 0.0, (FLOAT) 0.0, (FLOAT) 0.0);
+    PointInitialize(&TriangleVertex1, (FLOAT) 0.0, (FLOAT) 1.5, (FLOAT) 0.0);
+    PointInitialize(&TriangleVertex2, (FLOAT) 1.5, (FLOAT) 0.0, (FLOAT) 0.0);
+
+    Color3InitializeFromComponents(&Red, 
+                                   (FLOAT) 1.0, 
+                                   (FLOAT) 0.0, 
+                                   (FLOAT) 0.0);
+
+    Color3InitializeFromComponents(&Green, 
+                                   (FLOAT) 0.0, 
+                                   (FLOAT) 1.0, 
+                                   (FLOAT) 0.0);
+
+    Color3InitializeFromComponents(&Blue, 
+                                   (FLOAT) 0.0, 
+                                   (FLOAT) 0.0, 
+                                   (FLOAT) 1.0);
+
+    ConstantShader0 = ConstantEmissiveShaderAllocate(&Red);
+    ConstantShader1 = ConstantEmissiveShaderAllocate(&Green);
+    ConstantShader2 = ConstantEmissiveShaderAllocate(&Blue);
+
+    InterpolatedEmissiveShader = InterpolatedTriangleEmissiveShaderAllocate(ConstantShader0,
+                                                                            ConstantShader1,
+                                                                            ConstantShader2);
+
+    TriangleShader.EmissiveShader = InterpolatedEmissiveShader;
+    TriangleShader.DirectShader = NULL;
+    TriangleShader.IndirectShader = NULL;
+    TriangleShader.TranslucentShader = NULL;
+
+    Triangle = TriangleAllocate(&TriangleVertex0,
+                                &TriangleVertex1,
+                                &TriangleVertex2,
+                                &TriangleShader,
+                                NULL,
+                                NULL,
+                                NULL,
+                                NULL);
+
+    Scene = ListSceneAllocate();
+
+    Status = SceneAddWorldObject(Scene, Triangle);
+
+    Framebuffer = FramebufferAllocate(&Red, 500, 500);
+
+    RecursiveRayTracer = RecursiveNonRouletteRayTracerAllocate(Scene,
+                                                               (PRANDOM) Scene, // HACK
+                                                               (FLOAT) 0.0005,
+                                                               0);
+
+    PointInitialize(&PinholeLocation, (FLOAT) 0.0, (FLOAT) 0.0, (FLOAT) 4.0);
+    VectorInitialize(&CameraDirection, (FLOAT) 0.0, (FLOAT) 0.0, (FLOAT) -1.0);
+    VectorInitialize(&Up, (FLOAT) 0.0, (FLOAT) 1.0, (FLOAT) 0.0);
+
+    Status = PinholeCameraRender(&PinholeLocation,
+                                 (FLOAT) 1.0,
+                                 (FLOAT) 1.0,
+                                 (FLOAT) 1.0,
+                                 &CameraDirection,
+                                 &Up,
+                                 0,
+                                 500,
+                                 0,
+                                 0,
+                                 FALSE,
+                                 NULL,
+                                 RecursiveRayTracer,
+                                 Framebuffer);
+
+    CHECK_EQUAL(ISTATUS_SUCCESS, Status);
+
+    Success = WritePfm(Framebuffer, "RenderInterpolatedRedWorldTriangle.pfm");
 }
