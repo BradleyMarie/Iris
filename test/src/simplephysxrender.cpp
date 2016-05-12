4e7f2ee333044f861d81b715f72948469f290edb
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2016-05-11 18:08:02 -0700

    Move more tests to IrisPhysx

--- test/src/simplephysxrender.cpp
+++ test/src/simplephysxrender.cpp
@@ -39,12 +39,14 @@ public:
 
     void
     AddGeometry(
-        _In_ Geometry g
+        _In_ Geometry g,
+        _In_ Matrix m = Matrix::Identity(),
+        _In_ bool p = false
         )
     {
         GeometryList.push_back(g);
-        Matrices.push_back(Matrix::Identity());
-        Premultiplied.push_back(false);
+        Matrices.push_back(m);
+        Premultiplied.push_back(p);
     }
 
 private:
@@ -130,4 +132,162 @@ TEST(PhysxRenderConstantRedWorldSphere)
                           Fb);
 
     Fb.SaveAsPFM("RenderConstantRedWorldSpherePlusPlus.pfm");
+}
+
+TEST(PhysxRenderConstantRedModelSphere)
+{
+    Material ConstantMaterial = PhongMaterial::Create(Color3(1.0f, 0.0f, 0.0f),
+                                                      Color3::CreateBlack(),
+                                                      Color3::CreateBlack(),
+                                                      1.0f);
+
+    Geometry SphereRadiusOne = Sphere::Create(Point(0.0f, 0.0f, 0.0f),
+                                              1.0f,
+                                              ConstantMaterial,
+                                              ConstantMaterial,
+                                              nullopt,
+                                              nullopt);
+
+    TestListScene Scene;
+    Scene.AddGeometry(SphereRadiusOne, Matrix::Scalar(0.5f, 0.5f, 0.5f));
+
+    ProcessHitRoutine ProcessHitFunc = [](GeometryReference Geom,
+                                          UINT32 FaceHit,
+                                          Iris::MatrixReference ModelToWorld,
+                                          const Iris::Vector & ModelViewer,
+                                          const Iris::Point & ModelHitPoint,
+                                          const Iris::Point & WorldHitPoint,
+                                          const Iris::Ray & WorldRay,
+                                          PCPBR_LIGHT *PbrLights,
+                                          SIZE_T NumberOfLights,
+                                          IrisPhysx::RayTracer * RayTracerPtr,
+                                          VisibilityTester Tester,
+                                          BRDFAllocator Allocator,
+                                          IrisSpectrum::SpectrumCompositorReference SpectrumCompositor,
+                                          IrisSpectrum::ReflectorCompositorReference ReflectorCompositor,
+                                          IrisAdvanced::RandomReference Rng) -> SpectrumReference
+    {
+        Vector SurfaceNormal = Geom.ComputeNormal(ModelHitPoint, FaceHit);
+        MaterialReference Mat = *(Geom.GetMaterial(FaceHit));
+        BRDFReference Brdf = Mat.Sample(ModelHitPoint, nullptr, SurfaceNormal, ModelToWorld, Allocator);
+        auto Ref = Brdf.Sample(ModelViewer, Rng, ReflectorCompositor);
+        return SpectrumCompositor.Reflect(SpectrumReference(nullptr), std::get<0>(Ref));
+    };
+
+    ToneMappingRoutine ToneMappingFunc = [](SpectrumReference Spectra) -> Color3
+    {
+        return PhongToRGB(Spectra);
+    };
+
+    CreateStateRoutine CreateState = [&](std::vector<Random> & Rngs,
+                                         std::vector<ProcessHitRoutine> & ProcessHits,
+                                         std::vector<ToneMappingRoutine> & ToneMap,
+                                         SIZE_T NumberOfThreads)
+    {
+        for (SIZE_T Index = 0; Index < NumberOfThreads; Index++)
+        {
+            Rngs.push_back(MultiplyWithCarry::Create());
+            ProcessHits.push_back(ProcessHitFunc);
+            ToneMap.push_back(ToneMappingFunc);
+        }
+    };
+
+    Framebuffer Fb = Framebuffer::Create(Color3(0.0f, 0.0f, 0.0f), 500, 500);
+    PinholeCamera::Render(Point(0.0f, 0.0f, 4.0f),
+                          1.0f,
+                          1.0f,
+                          1.0f,
+                          Vector(0.0f, 0.0f, -1.0f),
+                          Vector(0.0f, 1.0f, 0.0f),
+                          0,
+                          0,
+                          0.0001f,
+                          0,
+                          false,
+                          false,
+                          Scene.GetTestRoutine(),
+                          nullopt,
+                          CreateState,
+                          Fb);
+
+    Fb.SaveAsPFM("RenderConstantRedModelSpherePlusPlus.pfm");
+}
+
+TEST(PhysxRenderConstantRedPremultipliedSphere)
+{
+    Material ConstantMaterial = PhongMaterial::Create(Color3(1.0f, 0.0f, 0.0f),
+                                                      Color3::CreateBlack(),
+                                                      Color3::CreateBlack(),
+                                                      1.0f);
+
+    Geometry SphereRadiusOne = Sphere::Create(Point(0.0f, 0.0f, 0.0f),
+                                              1.0f,
+                                              ConstantMaterial,
+                                              ConstantMaterial,
+                                              nullopt,
+                                              nullopt);
+
+    TestListScene Scene;
+    Scene.AddGeometry(SphereRadiusOne, Matrix::Scalar(1.0f, 1.0f, 1.0f), true);
+
+    ProcessHitRoutine ProcessHitFunc = [](GeometryReference Geom,
+                                          UINT32 FaceHit,
+                                          Iris::MatrixReference ModelToWorld,
+                                          const Iris::Vector & ModelViewer,
+                                          const Iris::Point & ModelHitPoint,
+                                          const Iris::Point & WorldHitPoint,
+                                          const Iris::Ray & WorldRay,
+                                          PCPBR_LIGHT *PbrLights,
+                                          SIZE_T NumberOfLights,
+                                          IrisPhysx::RayTracer * RayTracerPtr,
+                                          VisibilityTester Tester,
+                                          BRDFAllocator Allocator,
+                                          IrisSpectrum::SpectrumCompositorReference SpectrumCompositor,
+                                          IrisSpectrum::ReflectorCompositorReference ReflectorCompositor,
+                                          IrisAdvanced::RandomReference Rng) -> SpectrumReference
+    {
+        Vector SurfaceNormal = Geom.ComputeNormal(ModelHitPoint, FaceHit);
+        MaterialReference Mat = *(Geom.GetMaterial(FaceHit));
+        BRDFReference Brdf = Mat.Sample(ModelHitPoint, nullptr, SurfaceNormal, ModelToWorld, Allocator);
+        auto Ref = Brdf.Sample(ModelViewer, Rng, ReflectorCompositor);
+        return SpectrumCompositor.Reflect(SpectrumReference(nullptr), std::get<0>(Ref));
+    };
+
+    ToneMappingRoutine ToneMappingFunc = [](SpectrumReference Spectra) -> Color3
+    {
+        return PhongToRGB(Spectra);
+    };
+
+    CreateStateRoutine CreateState = [&](std::vector<Random> & Rngs,
+                                         std::vector<ProcessHitRoutine> & ProcessHits,
+                                         std::vector<ToneMappingRoutine> & ToneMap,
+                                         SIZE_T NumberOfThreads)
+    {
+        for (SIZE_T Index = 0; Index < NumberOfThreads; Index++)
+        {
+            Rngs.push_back(MultiplyWithCarry::Create());
+            ProcessHits.push_back(ProcessHitFunc);
+            ToneMap.push_back(ToneMappingFunc);
+        }
+    };
+
+    Framebuffer Fb = Framebuffer::Create(Color3(0.0f, 0.0f, 0.0f), 500, 500);
+    PinholeCamera::Render(Point(0.0f, 0.0f, 4.0f),
+                          1.0f,
+                          1.0f,
+                          1.0f,
+                          Vector(0.0f, 0.0f, -1.0f),
+                          Vector(0.0f, 1.0f, 0.0f),
+                          0,
+                          0,
+                          0.0001f,
+                          0,
+                          false,
+                          false,
+                          Scene.GetTestRoutine(),
+                          nullopt,
+                          CreateState,
+                          Fb);
+
+    Fb.SaveAsPFM("RenderConstantRedPremultipliedSpherePlusPlus.pfm");
 }
