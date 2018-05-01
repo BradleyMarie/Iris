0248393cb761d43e7a5e1d2e5547f45bcd1915d8
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2018-05-01 00:12:32 -0700

    Add ray tests.

---
+++ iris/ray_test.cc
@@ -0,0 +1,99 @@
+/*++
+
+Copyright (c) 2018 Brad Weinberger
+
+Module Name:
+
+    ray_test.cc
+
+Abstract:
+
+   Unit tests for ray.c
+
+--*/
+
+extern "C" {
+#include "iris/ray.h"
+}
+
+#include "googletest/include/gtest/gtest.h"
+#include "test/test_util.h"
+
+TEST(RayTest, RayCreate)
+{
+    POINT3 origin = PointCreate((float_t) 1.0, (float_t) 2.0, (float_t) 3.0);
+    VECTOR3 direction = VectorCreate((float_t) 4.0,
+                                     (float_t) 5.0, 
+                                     (float_t) 6.0);
+    RAY actual = RayCreate(origin, direction);
+
+    RAY expected;
+    expected.origin = origin;
+    expected.direction = direction;
+
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(RayTest, RayEndpoint)
+{
+    POINT3 origin = PointCreate((float_t) 1.0, (float_t) 2.0, (float_t) 3.0);
+    VECTOR3 direction = VectorCreate((float_t) 4.0,
+                                     (float_t) 5.0, 
+                                     (float_t) 6.0);
+    RAY ray = RayCreate(origin, direction);
+    POINT3 actual = RayEndpoint(ray, (float_t) 2.0);
+    
+    POINT3 expected = PointCreate((float_t) 9.0, 
+                                  (float_t) 12.0, 
+                                  (float_t) 15.0);
+
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(RayTest, RayNormalize)
+{
+    POINT3 origin = PointCreate((float_t) 1.0, (float_t) 2.0, (float_t) 3.0);
+    VECTOR3 direction = VectorCreate((float_t) 4.0,
+                                     (float_t) 5.0, 
+                                     (float_t) 6.0);
+    RAY ray = RayCreate(origin, direction);
+    RAY actual = RayNormalize(ray);
+    
+    EXPECT_EQ(origin, actual.origin);
+    EXPECT_NEAR((float_t) 0.455842, actual.direction.x, (float_t) 0.0001);
+    EXPECT_NEAR((float_t) 0.569802, actual.direction.y, (float_t) 0.0001);
+    EXPECT_NEAR((float_t) 0.683763, actual.direction.z, (float_t) 0.0001);
+}
+
+TEST(VectorTest, RayValidate)
+{
+    POINT3 origin = PointCreate((float_t) 0.0, (float_t) 0.0, (float_t) 0.0);
+    VECTOR3 direction = VectorCreate((float_t) 0.0, 
+                                     (float_t) 0.0, 
+                                     (float_t) 1.0);
+    RAY ray = RayCreate(origin, direction);
+    EXPECT_TRUE(RayValidate(ray));
+
+    ray.origin.x = (float_t) INFINITY;
+    EXPECT_FALSE(RayValidate(ray));
+    ray.origin.x = (float_t) 0.0;
+
+    ray.origin.y = (float_t) INFINITY;
+    EXPECT_FALSE(RayValidate(ray));
+    ray.origin.y = (float_t) 0.0;
+    
+    ray.origin.z = (float_t) INFINITY;
+    EXPECT_FALSE(RayValidate(ray));
+    ray.origin.z = (float_t) 0.0;
+
+    ray.direction.x = (float_t) INFINITY;
+    EXPECT_FALSE(RayValidate(ray));
+    ray.direction.x = (float_t) 0.0;
+
+    ray.direction.y = (float_t) INFINITY;
+    EXPECT_FALSE(RayValidate(ray));
+    ray.direction.y = (float_t) 0.0;
+    
+    ray.direction.z = (float_t) INFINITY;
+    EXPECT_FALSE(RayValidate(ray));
+}
--- test/test_util.h
+++ test/test_util.h
@@ -15,10 +15,10 @@ Abstract:
 #ifndef _TEST_TEST_UTIL_
 #define _TEST_TEST_UTIL_
 
-#include "iris/point.h"
-#include "iris/vector.h"
+#include "iris/ray.h"
 
-bool operator==(
+bool 
+operator==(
     const POINT3& p0, 
     const POINT3& p1
     )
@@ -26,12 +26,22 @@ bool operator==(
     return p0.x == p1.x && p0.y == p1.y && p0.z == p1.z;
 }
 
-bool operator==(
+bool 
+operator==(
     const VECTOR3& v0, 
     const VECTOR3& v1
     )
 {
     return v0.x == v1.x && v0.y == v1.y && v0.z == v1.z;
+}
+
+bool 
+operator==(
+    const RAY& r0, 
+    const RAY& r1
+    )
+{
+    return r0.origin == r1.origin && r0.direction == r1.direction;
 }
 
 #endif // _TEST_TEST_UTIL_
