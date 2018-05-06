168e98d6511d67ab87ddf335f1b072518d76011f
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2018-05-05 23:29:54 -0700

    Tweak build visibility and add Iris target

---
+++ iris/iris.h
@@ -0,0 +1,26 @@
+/*++
+
+Copyright (c) 2018 Brad Weinberger
+
+Module Name:
+
+    iris.h
+
+Abstract:
+
+    The public header file for Iris.
+
+--*/
+
+#ifndef _IRIS_
+#define _IRIS_
+
+#include "iris/matrix.h"
+#include "iris/multiply.h"
+#include "iris/point.h"
+#include "iris/ray.h"
+#include "iris/sal.h"
+#include "iris/status.h"
+#include "iris/vector.h"
+
+#endif _IRIS_
