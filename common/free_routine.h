b05dba673ae73b9451719c9a8ec20ed924058186
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2018-05-26 12:34:28 -0700

    Add free routine header.

---
+++ common/free_routine.h
@@ -0,0 +1,30 @@
+/*++
+
+Copyright (c) 2018 Brad Weinberger
+
+Module Name:
+
+    free_routine.h
+
+Abstract:
+
+    Function pointer type for free callbacks.
+
+--*/
+
+#ifndef _COMMON_FREE_ROUTINE_
+#define _COMMON_FREE_ROUTINE_
+
+#include "common/sal.h"
+
+//
+// Macros
+//
+
+typedef
+void
+(*PFREE_ROUTINE)(
+    _In_ _Post_invalid_ void *pointer
+    );
+
+#endif // _COMMON_FREE_ROUTINE_
