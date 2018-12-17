1c66d28eaf50fa1e87eefc8229402d9730566762
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2018-12-17 15:25:42 -0800

    Add initial cornell box test

---
+++ test_cases/cornell_box.cc
@@ -0,0 +1,350 @@
+/*++
+
+Copyright (c) 2018 Brad Weinberger
+
+Module Name:
+
+   cornell_box.cc
+
+Abstract:
+
+   Integration tests which render a single triangle.
+
+--*/
+
+#include <string>
+
+#include "iris_advanced_toolkit/pcg_random.h"
+#include "iris_camera_toolkit/grid_pixel_sampler.h"
+#include "iris_camera_toolkit/pfm_writer.h"
+#include "iris_camera_toolkit/pinhole_camera.h"
+#include "iris_physx_toolkit/all_light_sampler.h"
+#include "iris_physx_toolkit/attenuated_reflector.h"
+#include "iris_physx_toolkit/constant_emissive_material.h"
+#include "iris_physx_toolkit/constant_material.h"
+#include "iris_physx_toolkit/lambertian_brdf.h"
+#include "iris_physx_toolkit/list_scene.h"
+#include "iris_physx_toolkit/path_tracer.h"
+#include "iris_physx_toolkit/sample_tracer.h"
+#include "iris_physx_toolkit/triangle.h"
+#include "googletest/include/gtest/gtest.h"
+#include "test_util/cornell_box.h"
+#include "test_util/pfm.h"
+#include "test_util/quad.h"
+#include "test_util/spectra.h"
+
+void
+TestRenderSingleThreaded(
+    _In_ PCCAMERA camera,
+    _In_ PCLIST_SCENE scene,
+    _In_ PALL_LIGHT_SAMPLER light_sampler,
+    _In_ const std::string& file_name
+    )
+{
+    PPIXEL_SAMPLER pixel_sampler;
+    ISTATUS status =
+        GridPixelSamplerAllocate(1, 1, false, 1, 1, false, &pixel_sampler);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    PRANDOM rng;
+    status = PermutedCongruentialRandomAllocate(
+        0x853c49e6748fea9bULL,
+        0xda3e39cb94b95bdbULL,
+        &rng);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    PINTEGRATOR path_tracer;
+    status = PathTracerAllocate(0, 0, (float_t)0.0, &path_tracer);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    PTONE_MAPPER tone_mapper;
+    status = TestToneMapperAllocate(&tone_mapper);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    PSAMPLE_TRACER sample_tracer;
+    status = PhysxSampleTracerAllocate(
+        path_tracer,
+        ListSceneTraceCallback,
+        scene,
+        AllLightSamplerPrepareSamplesCallback,
+        AllLightSamplerNextSampleCallback,
+        light_sampler,
+        tone_mapper,
+        &sample_tracer);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    PFRAMEBUFFER framebuffer;
+    status = FramebufferAllocate(500, 500, &framebuffer);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    status = IrisCameraRender(
+        (float_t)0.001, camera, pixel_sampler, sample_tracer, rng, framebuffer);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    bool equals;
+    status = ExactlyEqualsPfmFile(framebuffer,
+                                  file_name.c_str(),
+                                  PFM_PIXEL_FORMAT_XYZ,
+                                  &equals);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+    EXPECT_TRUE(equals);
+
+    if (!equals)
+    {
+        status = WriteToPfmFile(framebuffer,
+                                file_name.c_str(),
+                                PFM_PIXEL_FORMAT_XYZ);
+        ASSERT_EQ(status, ISTATUS_SUCCESS);
+    }
+
+    PixelSamplerFree(pixel_sampler);
+    RandomFree(rng);
+    SampleTracerFree(sample_tracer);
+    FramebufferFree(framebuffer);
+}
+
+static
+void
+AddQuadToScene(
+    _In_ POINT3 v0,
+    _In_ POINT3 v1,
+    _In_ POINT3 v2,
+    _In_ POINT3 v3,
+    _In_opt_ PMATERIAL front_material,
+    _In_opt_ PMATERIAL back_material,
+    _Inout_ PLIST_SCENE scene
+    )
+{
+    PSHAPE shape0, shape1;
+    ISTATUS status = EmissiveQuadAllocate(
+        v0,
+        v1,
+        v2,
+        v3,
+        front_material,
+        back_material,
+        nullptr,
+        nullptr,
+        &shape0,
+        &shape1);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    status = ListSceneAddShape(scene, shape0);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    status = ListSceneAddShape(scene, shape1);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    ShapeRelease(shape0);
+    ShapeRelease(shape1);
+}
+
+TEST(CornellBoxTest, CornellBox)
+{
+    PREFLECTOR white_reflector;
+    ISTATUS status = TestReflectorAllocate((float_t)0.75,
+                                           (float_t)0.75,
+                                           (float_t)0.75,
+                                           &white_reflector);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    PBRDF white_brdf;
+    status = LambertianBrdfAllocate(white_reflector, &white_brdf);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    PMATERIAL white_material;
+    status = ConstantMaterialAllocate(white_brdf, &white_material);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    PREFLECTOR red_reflector;
+    status = TestReflectorAllocate((float_t)0.75,
+                                   (float_t)0.25,
+                                   (float_t)0.25,
+                                   &red_reflector);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    PBRDF red_brdf;
+    status = LambertianBrdfAllocate(red_reflector, &red_brdf);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    PMATERIAL red_material;
+    status = ConstantMaterialAllocate(red_brdf, &red_material);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    PREFLECTOR blue_reflector;
+    status = TestReflectorAllocate((float_t)0.25,
+                                   (float_t)0.25,
+                                   (float_t)0.75,
+                                   &blue_reflector);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    PBRDF blue_brdf;
+    status = LambertianBrdfAllocate(blue_reflector, &blue_brdf);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    PMATERIAL blue_material;
+    status = ConstantMaterialAllocate(blue_brdf, &blue_material);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    PSPECTRUM light_spectrum;
+    status = TestSpectrumAllocate((float_t)12.0,
+                                  (float_t)12.0,
+                                  (float_t)12.0,
+                                  &light_spectrum);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    PEMISSIVE_MATERIAL light_material;
+    status = ConstantEmissiveMaterialAllocate(light_spectrum, &light_material);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    PLIST_SCENE scene;
+    status = ListSceneAllocate(&scene);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    PALL_LIGHT_SAMPLER light_sampler;
+    status = AllLightSamplerAllocate(&light_sampler);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    PSHAPE light_shape0, light_shape1;
+    status = EmissiveQuadAllocate(
+        cornell_box_light[0],
+        cornell_box_light[1],
+        cornell_box_light[2],
+        cornell_box_light[3],
+        nullptr,
+        nullptr,
+        light_material,
+        nullptr,
+        &light_shape0,
+        &light_shape1);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    status = ListSceneAddShape(scene, light_shape0);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    status = ListSceneAddShape(scene, light_shape1);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    PLIGHT light0;
+    status = AreaLightAllocate(light_shape0,
+                               TRIANGLE_FRONT_FACE,
+                               nullptr,
+                               &light0);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    status = AllLightSamplerAddLight(light_sampler, light0);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    PLIGHT light1;
+    status = AreaLightAllocate(light_shape1,
+                               TRIANGLE_FRONT_FACE,
+                               nullptr,
+                               &light1);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    status = AllLightSamplerAddLight(light_sampler, light1);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    AddQuadToScene(
+        cornell_box_ceiling[0],
+        cornell_box_ceiling[1],
+        cornell_box_ceiling[2],
+        cornell_box_ceiling[3],
+        white_material,
+        white_material,
+        scene);
+
+    AddQuadToScene(
+        cornell_box_back_wall[0],
+        cornell_box_back_wall[1],
+        cornell_box_back_wall[2],
+        cornell_box_back_wall[3],
+        white_material,
+        white_material,
+        scene);
+                
+    AddQuadToScene(
+        cornell_box_floor[0],
+        cornell_box_floor[1],
+        cornell_box_floor[2],
+        cornell_box_floor[3],
+        white_material,
+        white_material,
+        scene);
+
+    AddQuadToScene(
+        cornell_box_left_wall[0],
+        cornell_box_left_wall[1],
+        cornell_box_left_wall[2],
+        cornell_box_left_wall[3],
+        red_material,
+        red_material,
+        scene);
+
+    AddQuadToScene(
+        cornell_box_right_wall[0],
+        cornell_box_right_wall[1],
+        cornell_box_right_wall[2],
+        cornell_box_right_wall[3],
+        blue_material,
+        blue_material,
+        scene);
+
+    for (size_t i = 0; i < 5; i++)
+    {
+        AddQuadToScene(
+            cornell_box_short_box[i][0],
+            cornell_box_short_box[i][1],
+            cornell_box_short_box[i][2],
+            cornell_box_short_box[i][3],
+            white_material,
+            white_material,
+            scene);
+    }
+
+    for (size_t i = 0; i < 5; i++)
+    {
+        AddQuadToScene(
+            cornell_box_tall_box[i][0],
+            cornell_box_tall_box[i][1],
+            cornell_box_tall_box[i][2],
+            cornell_box_tall_box[i][3],
+            white_material,
+            white_material,
+            scene);
+    }
+
+    PCAMERA camera;
+    status = PinholeCameraAllocate(
+        cornell_box_camera_location,
+        cornell_box_camera_direction,
+        cornell_box_camera_up,
+        cornell_box_focal_length,
+        cornell_box_camera_width,
+        cornell_box_camera_height,
+        &camera);
+    ASSERT_EQ(status, ISTATUS_SUCCESS);
+
+    TestRenderSingleThreaded(camera,
+                             scene,
+                             light_sampler,
+                             "test_results/blank.pfm");
+
+    SpectrumRelease(light_spectrum);
+    ReflectorRelease(white_reflector);
+    ReflectorRelease(red_reflector);
+    ReflectorRelease(blue_reflector);
+    BrdfRelease(white_brdf);
+    BrdfRelease(red_brdf);
+    BrdfRelease(blue_brdf);
+    MaterialRelease(white_material);
+    MaterialRelease(red_material);
+    MaterialRelease(blue_material);
+    ShapeRelease(light_shape0);
+    ShapeRelease(light_shape1);
+    LightRelease(light0);
+    LightRelease(light1);
+    ListSceneFree(scene);
+    AllLightSamplerFree(light_sampler);
+    CameraFree(camera);
+}
