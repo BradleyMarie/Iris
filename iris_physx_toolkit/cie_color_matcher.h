be52d40ecb44c9867d40f0b2d40fb9787936f83c
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2019-01-15 01:03:07 -0800

    Implement CIE color matcher

---
+++ iris_physx_toolkit/cie_color_matcher.h
@@ -0,0 +1,38 @@
+/*++
+
+Copyright (c) 2018 Brad Weinberger
+
+Module Name:
+
+    cie_color_matcher.h
+
+Abstract:
+
+    Creates a color matcher which generates a color using the CIE 1931 2 degree
+    standard observer sampled once per nanometer.
+
+--*/
+
+#ifndef _IRIS_PHYSX_TOOLKIT_CIE_COLOR_MATCHER_
+#define _IRIS_PHYSX_TOOLKIT_CIE_COLOR_MATCHER_
+
+#include "iris_physx/iris_physx.h"
+
+#if __cplusplus 
+extern "C" {
+#endif // __cplusplus
+
+//
+// Functions
+//
+
+ISTATUS
+CieColorMatcherAllocate(
+    _Out_ PCOLOR_MATCHER *color_matcher
+    );
+
+#if __cplusplus 
+}
+#endif // __cplusplus
+
+#endif // _IRIS_PHYSX_TOOLKIT_CIE_COLOR_MATCHER_
