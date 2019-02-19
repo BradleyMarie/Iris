54566d618c3116e85e6a6ec0f0342af141d2ff78
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2019-02-18 22:32:09 -0800

    Add Mirror BSDF

---
+++ iris_physx_toolkit/mirror_bsdf.c
@@ -0,0 +1,184 @@
+/*++
+
+Copyright (c) 2019 Brad Weinberger
+
+Module Name:
+
+    mirror_bsdf.c
+
+Abstract:
+
+    Implements a mirror BSDF.
+
+--*/
+
+#include <stdalign.h>
+
+#include "iris_physx_toolkit/attenuated_reflector.h"
+#include "iris_physx_toolkit/mirror_bsdf.h"
+
+//
+// Types
+//
+
+typedef struct _MIRROR_BSDF {
+    PREFLECTOR reflector;
+} MIRROR_BSDF, *PMIRROR_BSDF;
+
+typedef const MIRROR_BSDF *PCMIRROR_BSDF;
+
+//
+// Static Functions
+//
+
+static
+ISTATUS
+MirrorReflectorSample(
+    _In_ const void *context,
+    _In_ VECTOR3 incoming,
+    _In_ VECTOR3 normal,
+    _Inout_ PRANDOM rng,
+    _Inout_ PREFLECTOR_COMPOSITOR compositor,
+    _Out_ PCREFLECTOR *reflector,
+    _Out_ PVECTOR3 outgoing,
+    _Out_ float_t *pdf
+    )
+{
+    PCMIRROR_BSDF mirror_bsdf = (PCMIRROR_BSDF)context;
+
+    *reflector = mirror_bsdf->reflector;
+    *outgoing = VectorReflect(incoming, normal);
+    *pdf = INFINITY;
+
+    return ISTATUS_SUCCESS;
+}
+
+static
+ISTATUS
+MirrorReflectorComputeReflectance(
+    _In_ const void *context,
+    _In_ VECTOR3 incoming,
+    _In_ VECTOR3 normal,
+    _In_ VECTOR3 outgoing,
+    _Inout_ PREFLECTOR_COMPOSITOR compositor,
+    _Out_ PCREFLECTOR *reflector
+    )
+{
+    *reflector = NULL;
+
+    return ISTATUS_SUCCESS;
+}
+
+static
+ISTATUS
+MirrorReflectorComputeReflectanceWithPdf(
+    _In_ const void *context,
+    _In_ VECTOR3 incoming,
+    _In_ VECTOR3 normal,
+    _In_ VECTOR3 outgoing,
+    _Inout_ PREFLECTOR_COMPOSITOR compositor,
+    _Out_ PCREFLECTOR *reflector,
+    _Out_ float_t *pdf
+    )
+{
+    *pdf = (float_t)0.0;
+
+    return ISTATUS_SUCCESS;
+}
+
+
+static
+void
+MirrorReflectorFree(
+    _In_opt_ _Post_invalid_ void *context
+    )
+{
+    PMIRROR_BSDF mirror_bsdf = (PMIRROR_BSDF)context;
+
+    ReflectorRelease(mirror_bsdf->reflector);
+}
+
+//
+// Static Variables
+//
+
+static const BSDF_VTABLE mirror_bsdf_vtable = {
+    MirrorReflectorSample,
+    MirrorReflectorComputeReflectance,
+    MirrorReflectorComputeReflectanceWithPdf,
+    MirrorReflectorFree
+};
+
+//
+// Functions
+//
+
+ISTATUS
+MirrorReflectorAllocate(
+    _In_ PREFLECTOR reflector,
+    _Out_ PBSDF *bsdf
+    )
+{
+    if (reflector == NULL)
+    {
+        return ISTATUS_INVALID_ARGUMENT_00;
+    }
+
+    if (bsdf == NULL)
+    {
+        return ISTATUS_INVALID_ARGUMENT_01;
+    }
+
+    MIRROR_BSDF mirror_bsdf;
+    mirror_bsdf.reflector = reflector;
+
+    ISTATUS status = BsdfAllocate(&mirror_bsdf_vtable,
+                                  &mirror_bsdf,
+                                  sizeof(MIRROR_BSDF),
+                                  alignof(MIRROR_BSDF),
+                                  bsdf);
+
+    if (status != ISTATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    ReflectorRetain(reflector);
+
+    return ISTATUS_SUCCESS;
+}
+
+ISTATUS
+MirrorReflectorAllocateWithAllocator(
+    _Inout_ PBSDF_ALLOCATOR bsdf_allocator,
+    _In_ PCREFLECTOR reflector,
+    _Out_ PCBSDF *bsdf
+    )
+{
+    if (bsdf_allocator == NULL)
+    {
+        return ISTATUS_INVALID_ARGUMENT_00;
+    }
+
+    if (reflector == NULL)
+    {
+        return ISTATUS_INVALID_ARGUMENT_01;
+    }
+
+    if (bsdf == NULL)
+    {
+        return ISTATUS_INVALID_ARGUMENT_02;
+    }
+
+    MIRROR_BSDF mirror_bsdf;
+    mirror_bsdf.reflector = (PREFLECTOR)reflector;
+
+    ISTATUS status = BsdfAllocatorAllocate(bsdf_allocator,
+                                           &mirror_bsdf_vtable,
+                                           &mirror_bsdf,
+                                           sizeof(MIRROR_BSDF),
+                                           alignof(MIRROR_BSDF),
+                                           bsdf);
+
+    return status;   
+}
---
+++ iris_physx_toolkit/mirror_bsdf.h
@@ -0,0 +1,45 @@
+/*++
+
+Copyright (c) 2019 Brad Weinberger
+
+Module Name:
+
+    mirror_bsdf.h
+
+Abstract:
+
+    Creates a mirror BSDF.
+
+--*/
+
+#ifndef _IRIS_PHYSX_TOOLKIT_MIRROR_BSDF_
+#define _IRIS_PHYSX_TOOLKIT_MIRROR_BSDF_
+
+#include "iris_physx/iris_physx.h"
+
+#if __cplusplus 
+extern "C" {
+#endif // __cplusplus
+
+//
+// Functions
+//
+
+ISTATUS
+MirrorReflectorAllocate(
+    _In_ PREFLECTOR reflector,
+    _Out_ PBSDF *bsdf
+    );
+
+ISTATUS
+MirrorReflectorAllocateWithAllocator(
+    _Inout_ PBSDF_ALLOCATOR bsdf_allocator,
+    _In_ PCREFLECTOR reflector,
+    _Out_ PCBSDF *bsdf
+    );
+
+#if __cplusplus 
+}
+#endif // __cplusplus
+
+#endif // _IRIS_PHYSX_TOOLKIT_MIRROR_BSDF_
