cafa5b84ccb0478a77ce868c6b1ba0c7e5721367
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2019-12-28 15:56:37 -0800

    Add float mipmap

--- iris_physx_toolkit/mipmap.c
+++ iris_physx_toolkit/mipmap.c
@@ -22,7 +22,7 @@ Abstract:
 // Types
 //
 
-struct _MIPMAP {
+struct _SPECTRUM_MIPMAP {
     _Field_size_(width * height) PREFLECTOR *textels;
     float_t width_fp;
     float_t height_fp;
@@ -35,12 +35,13 @@ struct _MIPMAP {
 //
 
 ISTATUS
-MipmapAllocate(
+SpectrumMipmapAllocate(
     _In_reads_(height * width) float_t textels[][3],
     _In_ size_t width,
     _In_ size_t height,
     _In_ PCRGB_INTERPOLATOR rgb_interpolator,
-    _Out_ PMIPMAP *mipmap
+    _Inout_opt_ PCOLOR_INTEGRATOR color_integrator,
+    _Out_ PSPECTRUM_MIPMAP *mipmap
     )
 {
     if (textels == NULL)
@@ -63,9 +64,14 @@ MipmapAllocate(
         return ISTATUS_INVALID_ARGUMENT_03;
     }
 
-    if (mipmap == NULL)
+    if (color_integrator == NULL)
     {
         return ISTATUS_INVALID_ARGUMENT_04;
+    }
+
+    if (mipmap == NULL)
+    {
+        return ISTATUS_INVALID_ARGUMENT_05;
     }
 
     size_t num_pixels;
@@ -76,7 +82,7 @@ MipmapAllocate(
         return ISTATUS_ALLOCATION_FAILED;
     }
 
-    PMIPMAP result = (PMIPMAP)malloc(sizeof(MIPMAP));
+    PSPECTRUM_MIPMAP result = (PSPECTRUM_MIPMAP)malloc(sizeof(SPECTRUM_MIPMAP));
 
     if (result == NULL)
     {
@@ -101,19 +107,19 @@ MipmapAllocate(
     {
         if (!isfinite(textels[i][0]) || textels[i][0] < (float_t)0.0)
         {
-            MipmapFree(result);
+            SpectrumMipmapFree(result);
             return ISTATUS_INVALID_ARGUMENT_01;
         }
 
         if (!isfinite(textels[i][1]) || textels[i][1] < (float_t)0.0)
         {
-            MipmapFree(result);
+            SpectrumMipmapFree(result);
             return ISTATUS_INVALID_ARGUMENT_02;
         }
 
         if (!isfinite(textels[i][2]) || textels[i][2] < (float_t)0.0)
         {
-            MipmapFree(result);
+            SpectrumMipmapFree(result);
             return ISTATUS_INVALID_ARGUMENT_03;
         }
 
@@ -126,7 +132,22 @@ MipmapAllocate(
         if (status != ISTATUS_SUCCESS)
         {
             assert(status == ISTATUS_ALLOCATION_FAILED);
-            MipmapFree(result);
+            SpectrumMipmapFree(result);
+            return status;
+        }
+
+        if (color_integrator == NULL)
+        {
+            continue;
+        }
+
+        status = ColorIntegratorPrecomputeReflectorColor(color_integrator,
+                                                         result->textels[i]);
+
+        if (status != ISTATUS_SUCCESS)
+        {
+            assert(status == ISTATUS_ALLOCATION_FAILED);
+            SpectrumMipmapFree(result);
             return status;
         }
     }
@@ -137,8 +158,8 @@ MipmapAllocate(
 }
 
 ISTATUS
-MipmapLookup(
-    _In_ PCMIPMAP mipmap,
+SpectrumMipmapLookup(
+    _In_ PCSPECTRUM_MIPMAP mipmap,
     _In_ float_t s,
     _In_ float_t t,
     _In_ float_t dsdx,
@@ -216,8 +237,8 @@ MipmapLookup(
 }
 
 void
-MipmapFree(
-    _In_opt_ _Post_invalid_ PMIPMAP mipmap
+SpectrumMipmapFree(
+    _In_opt_ _Post_invalid_ PSPECTRUM_MIPMAP mipmap
     )
 {
     if (mipmap == NULL)
@@ -229,6 +250,194 @@ MipmapFree(
     for (size_t i = 0; i < num_pixels; i++)
     {
         ReflectorRelease(mipmap->textels[i]);
+    }
+
+    free(mipmap->textels);
+    free(mipmap);
+}
+
+//
+// Types
+//
+
+struct _FLOAT_MIPMAP {
+    _Field_size_(width * height) float_t *textels;
+    float_t width_fp;
+    float_t height_fp;
+    size_t width;
+    size_t height;
+};
+
+//
+// Functions
+//
+
+ISTATUS
+FloatMipmapAllocate(
+    _In_reads_(height * width) float_t textels[],
+    _In_ size_t width,
+    _In_ size_t height,
+    _Out_ PFLOAT_MIPMAP *mipmap
+    )
+{
+    if (textels == NULL)
+    {
+        return ISTATUS_INVALID_ARGUMENT_00;
+    }
+
+    if (width == 0)
+    {
+        return ISTATUS_INVALID_ARGUMENT_01;
+    }
+
+    if (height == 0)
+    {
+        return ISTATUS_INVALID_ARGUMENT_02;
+    }
+
+    if (mipmap == NULL)
+    {
+        return ISTATUS_INVALID_ARGUMENT_03;
+    }
+
+    size_t num_pixels;
+    bool success = CheckedMultiplySizeT(width, height, &num_pixels);
+
+    if (!success)
+    {
+        return ISTATUS_ALLOCATION_FAILED;
+    }
+
+    PFLOAT_MIPMAP result = (PFLOAT_MIPMAP)malloc(sizeof(FLOAT_MIPMAP));
+
+    if (result == NULL)
+    {
+        return ISTATUS_ALLOCATION_FAILED;
+    }
+
+    float_t *spectra = (float_t*)calloc(num_pixels, sizeof(float_t));
+
+    if (spectra == NULL)
+    {
+        free(result);
+        return ISTATUS_ALLOCATION_FAILED;
+    }
+
+    result->textels = spectra;
+    result->width = width;
+    result->height = height;
+    result->width_fp = (float_t)width;
+    result->height_fp = (float_t)height;
+
+    for (size_t i = 0; i < num_pixels; i++)
+    {
+        if (!isfinite(textels[i]) || textels[i] < (float_t)0.0)
+        {
+            FloatMipmapFree(result);
+            return ISTATUS_INVALID_ARGUMENT_01;
+        }
+
+        if (!isfinite(textels[i]) || textels[i] < (float_t)0.0)
+        {
+            FloatMipmapFree(result);
+            return ISTATUS_INVALID_ARGUMENT_02;
+        }
+
+        if (!isfinite(textels[i]) || textels[i] < (float_t)0.0)
+        {
+            FloatMipmapFree(result);
+            return ISTATUS_INVALID_ARGUMENT_03;
+        }
+
+        result->textels[i] = textels[i];
+    }
+
+    *mipmap = result;
+
+    return ISTATUS_SUCCESS;
+}
+
+ISTATUS
+FloatMipmapLookup(
+    _In_ PCFLOAT_MIPMAP mipmap,
+    _In_ float_t s,
+    _In_ float_t t,
+    _In_ float_t dsdx,
+    _In_ float_t dsdy,
+    _In_ float_t dtdx,
+    _In_ float_t dtdy,
+    _Out_ float_t *value
+    )
+{
+    if (mipmap == NULL)
+    {
+        return ISTATUS_INVALID_ARGUMENT_00;
+    }
+
+    if (!isfinite(s))
+    {
+        return ISTATUS_INVALID_ARGUMENT_01;
+    }
+
+    if (!isfinite(t))
+    {
+        return ISTATUS_INVALID_ARGUMENT_02;
+    }
+
+    if (isnan(dsdx))
+    {
+        return ISTATUS_INVALID_ARGUMENT_03;
+    }
+
+    if (isnan(dsdy))
+    {
+        return ISTATUS_INVALID_ARGUMENT_04;
+    }
+
+    if (isnan(dtdx))
+    {
+        return ISTATUS_INVALID_ARGUMENT_05;
+    }
+
+    if (isnan(dtdy))
+    {
+        return ISTATUS_INVALID_ARGUMENT_06;
+    }
+
+    if (value == NULL)
+    {
+        return ISTATUS_INVALID_ARGUMENT_07;
+    }
+
+    float_t x = floor(mipmap->width_fp * s);
+
+    if (x < (float_t)0.0 || mipmap->width <= x)
+    {
+        *value = (float_t)0.0;
+        return ISTATUS_SUCCESS;
+    }
+
+    float_t y = floor(mipmap->height_fp * t);
+
+    if (y < (float_t)0.00 || mipmap->height <= y)
+    {
+        *value = (float_t)0.0;
+        return ISTATUS_SUCCESS;
+    }
+
+    *value = mipmap->textels[(size_t)y * mipmap->width + (size_t)x];
+
+    return ISTATUS_SUCCESS;
+}
+
+void
+FloatMipmapFree(
+    _In_opt_ _Post_invalid_ PFLOAT_MIPMAP mipmap
+    )
+{
+    if (mipmap == NULL)
+    {
+        return;
     }
 
     free(mipmap->textels);
--- iris_physx_toolkit/mipmap.h
+++ iris_physx_toolkit/mipmap.h
@@ -25,25 +25,26 @@ extern "C" {
 // Types
 //
 
-typedef struct _MIPMAP MIPMAP, *PMIPMAP;
-typedef const MIPMAP *PCMIPMAP;
+typedef struct _SPECTRUM_MIPMAP SPECTRUM_MIPMAP, *PSPECTRUM_MIPMAP;
+typedef const SPECTRUM_MIPMAP *PCSPECTRUM_MIPMAP;
 
 //
 // Functions
 //
 
 ISTATUS
-MipmapAllocate(
+SpectrumMipmapAllocate(
     _In_reads_(height * width) float_t textels[][3],
     _In_ size_t width,
     _In_ size_t height,
     _In_ PCRGB_INTERPOLATOR rgb_interpolator,
-    _Out_ PMIPMAP *mipmap
+    _Inout_opt_ PCOLOR_INTEGRATOR color_integrator,
+    _Out_ PSPECTRUM_MIPMAP *mipmap
     );
 
 ISTATUS
-MipmapLookup(
-    _In_ PCMIPMAP mipmap,
+SpectrumMipmapLookup(
+    _In_ PCSPECTRUM_MIPMAP mipmap,
     _In_ float_t s,
     _In_ float_t t,
     _In_ float_t dsdx,
@@ -55,12 +56,48 @@ MipmapLookup(
     );
 
 void
-MipmapFree(
-    _In_opt_ _Post_invalid_ PMIPMAP mipmap
+SpectrumMipmapFree(
+    _In_opt_ _Post_invalid_ PSPECTRUM_MIPMAP mipmap
+    );
+
+//
+// Types
+//
+
+typedef struct _FLOAT_MIPMAP FLOAT_MIPMAP, *PFLOAT_MIPMAP;
+typedef const FLOAT_MIPMAP *PCFLOAT_MIPMAP;
+
+//
+// Functions
+//
+
+ISTATUS
+FloatMipmapAllocate(
+    _In_reads_(height * width) float_t textels[],
+    _In_ size_t width,
+    _In_ size_t height,
+    _Out_ PFLOAT_MIPMAP *mipmap
+    );
+
+ISTATUS
+FloatMipmapLookup(
+    _In_ PCFLOAT_MIPMAP mipmap,
+    _In_ float_t s,
+    _In_ float_t t,
+    _In_ float_t dsdx,
+    _In_ float_t dsdy,
+    _In_ float_t dtdx,
+    _In_ float_t dtdy,
+    _Out_ float_t *value
+    );
+
+void
+FloatMipmapFree(
+    _In_opt_ _Post_invalid_ PFLOAT_MIPMAP mipmap
     );
 
 #if __cplusplus 
 }
 #endif // __cplusplus
 
-#endif // _IRIS_PHYSX_TOOLKIT_MIPMAP_
+#endif // _IRIS_PHYSX_TOOLKIT_SPECTRUM_MIPMAP_
