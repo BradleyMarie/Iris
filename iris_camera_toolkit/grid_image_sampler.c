460e2708e2274f64592ea5615035bef18fd750fd
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-02-08 11:39:06 -0800

    Simplify grid pixel sampler

--- iris_camera_toolkit/grid_image_sampler.c
+++ iris_camera_toolkit/grid_image_sampler.c
@@ -39,10 +39,6 @@ typedef struct _GRID_IMAGE_SAMPLER {
     float_t lens_sample_width_v;
     bool jitter_pixel_samples;
     bool jitter_lens_samples;
-    uint16_t current_pixel_samples_u;
-    uint16_t current_pixel_samples_v;
-    uint16_t current_lens_samples_u;
-    uint16_t current_lens_samples_v;
 } GRID_IMAGE_SAMPLER, *PGRID_IMAGE_SAMPLER;
 
 typedef const GRID_IMAGE_SAMPLER *PCGRID_IMAGE_SAMPLER;
@@ -50,6 +46,42 @@ typedef const GRID_IMAGE_SAMPLER *PCGRID_IMAGE_SAMPLER;
 //
 // Static Functions
 //
+
+static
+ISTATUS
+GridImageSampler2DOffsets(
+    _Inout_ PRANDOM rng,
+    _In_ float_t u_maximum,
+    _In_ float_t v_maximum,
+    _In_ bool jitter,
+    _Out_ float_t *u,
+    _Out_ float_t *v
+    )
+{
+    if (!jitter)
+    {
+        *u = (float_t)0.5 * u_maximum;
+        *v = (float_t)0.5 * v_maximum;
+        return ISTATUS_SUCCESS;
+    }
+
+    ISTATUS status = RandomGenerateFloat(rng,
+                                         (float_t)0.0,
+                                         u_maximum,
+                                         u);
+
+    if (status != ISTATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    status = RandomGenerateFloat(rng,
+                                 (float_t)0.0,
+                                 v_maximum,
+                                 v);
+
+    return status;
+}
 
 static
 ISTATUS
@@ -70,46 +102,6 @@ GridImageSamplerPreparePixelSamples(
 {
     PGRID_IMAGE_SAMPLER image_sampler = (PGRID_IMAGE_SAMPLER)context;
 
-    if (pixel_min_u == pixel_max_u)
-    {
-        image_sampler->current_pixel_samples_u = 1;
-    }
-    else
-    {
-        image_sampler->current_pixel_samples_u =
-            image_sampler->pixel_samples_u;
-    }
-
-    if (pixel_min_v == pixel_max_v)
-    {
-        image_sampler->current_pixel_samples_v = 1;
-    }
-    else
-    {
-        image_sampler->current_pixel_samples_v =
-            image_sampler->pixel_samples_v;
-    }
-
-    if (lens_min_u == lens_max_u)
-    {
-        image_sampler->current_lens_samples_u = 1;
-    }
-    else
-    {
-        image_sampler->current_lens_samples_u =
-            image_sampler->lens_samples_u;
-    }
-
-    if (lens_min_v == lens_max_v)
-    {
-        image_sampler->current_lens_samples_v = 1;
-    }
-    else
-    {
-        image_sampler->current_lens_samples_v =
-            image_sampler->lens_samples_v;
-    }
-
     image_sampler->pixel_min_u = pixel_min_u;
     image_sampler->pixel_min_v = pixel_min_v;
     image_sampler->lens_min_u = lens_min_u;
@@ -128,10 +120,10 @@ GridImageSamplerPreparePixelSamples(
     image_sampler->lens_sample_width_v =
         (lens_max_v - lens_min_v) / (float_t)image_sampler->lens_samples_v;
 
-    *num_samples = image_sampler->current_pixel_samples_u *
-                   image_sampler->current_pixel_samples_v *
-                   image_sampler->current_lens_samples_u *
-                   image_sampler->current_lens_samples_v;
+    *num_samples = image_sampler->pixel_samples_u *
+                   image_sampler->pixel_samples_v *
+                   image_sampler->lens_samples_u *
+                   image_sampler->lens_samples_v;
 
     return ISTATUS_SUCCESS;
 }
@@ -153,89 +145,57 @@ GridImageSamplerNextSample(
     PGRID_IMAGE_SAMPLER image_sampler = (PGRID_IMAGE_SAMPLER)context;
 
     float_t pixel_jitter_u, pixel_jitter_v;
-    if (image_sampler->jitter_pixel_samples)
+    ISTATUS status = GridImageSampler2DOffsets(rng,
+                                               image_sampler->pixel_sample_width_u,
+                                               image_sampler->pixel_sample_width_v,
+                                               image_sampler->jitter_pixel_samples,
+                                               &pixel_jitter_u,
+                                               &pixel_jitter_v);
+
+    if (status != ISTATUS_SUCCESS)
     {
-        ISTATUS status = RandomGenerateFloat(rng,
-                                             (float_t)0.0,
-                                             image_sampler->pixel_sample_width_u,
-                                             &pixel_jitter_u);
-
-        if (status != ISTATUS_SUCCESS)
-        {
-            return status;
-        }
-
-        status = RandomGenerateFloat(rng,
-                                     (float_t)0.0,
-                                     image_sampler->pixel_sample_width_v,
-                                     &pixel_jitter_v);
-
-        if (status != ISTATUS_SUCCESS)
-        {
-            return status;
-        }
+        return status;
     }
-    else
+
+    float_t lens_jitter_u, lens_jitter_v;
+    status = GridImageSampler2DOffsets(rng,
+                                       image_sampler->lens_sample_width_u,
+                                       image_sampler->lens_sample_width_v,
+                                       image_sampler->jitter_lens_samples,
+                                       &lens_jitter_u,
+                                       &lens_jitter_v);
+
+    if (status != ISTATUS_SUCCESS)
     {
-        pixel_jitter_u = (float_t)0.5 * image_sampler->pixel_sample_width_u;
-        pixel_jitter_v = (float_t)0.5 * image_sampler->pixel_sample_width_v;
+        return status;
     }
 
     size_t pixel_u_index =
-        sample_index % image_sampler->current_pixel_samples_u;
+        sample_index % image_sampler->pixel_samples_u;
     pixel_jitter_u +=
         image_sampler->pixel_min_u + (float_t)pixel_u_index * image_sampler->pixel_sample_width_u;
     *pixel_sample_u = IMax(IMin(pixel_jitter_u, image_sampler->pixel_max_u),
                            image_sampler->pixel_min_u);
-    size_t sample_divisor = image_sampler->current_pixel_samples_u;
+    size_t sample_divisor = image_sampler->pixel_samples_u;
 
     size_t pixel_v_index =
-        (sample_index / sample_divisor) % image_sampler->current_pixel_samples_v;
+        (sample_index / sample_divisor) % image_sampler->pixel_samples_v;
     pixel_jitter_v +=
         image_sampler->pixel_min_v + (float_t)pixel_v_index * image_sampler->pixel_sample_width_v;
     *pixel_sample_v = IMax(IMin(pixel_jitter_v, image_sampler->pixel_max_v),
                            image_sampler->pixel_min_v);
-    sample_divisor *= image_sampler->current_pixel_samples_v;
-
-    float_t lens_jitter_u, lens_jitter_v;
-    if (image_sampler->jitter_lens_samples)
-    {
-        ISTATUS status = RandomGenerateFloat(rng,
-                                             (float_t)0.0,
-                                             image_sampler->lens_sample_width_u,
-                                             &lens_jitter_u);
-
-        if (status != ISTATUS_SUCCESS)
-        {
-            return status;
-        }
-
-        status = RandomGenerateFloat(rng,
-                                     (float_t)0.0,
-                                     image_sampler->lens_sample_width_v,
-                                     &lens_jitter_v);
-
-        if (status != ISTATUS_SUCCESS)
-        {
-            return status;
-        }
-    }
-    else
-    {
-        lens_jitter_u = (float_t)0.5 * image_sampler->lens_sample_width_u;
-        lens_jitter_v = (float_t)0.5 * image_sampler->lens_sample_width_v;
-    }
+    sample_divisor *= image_sampler->pixel_samples_v;
 
     size_t lens_u_index =
-        (sample_index / sample_divisor) % image_sampler->current_lens_samples_u;
+        (sample_index / sample_divisor) % image_sampler->lens_samples_u;
     lens_jitter_u +=
         image_sampler->lens_min_u + (float_t)lens_u_index * image_sampler->lens_sample_width_u;
     *lens_sample_u = IMax(IMin(pixel_jitter_u, image_sampler->lens_max_u),
                            image_sampler->lens_min_u);
-    sample_divisor *= image_sampler->current_lens_samples_u;
+    sample_divisor *= image_sampler->lens_samples_u;
 
     size_t lens_v_index =
-        (sample_index / sample_divisor) % image_sampler->current_lens_samples_v;
+        (sample_index / sample_divisor) % image_sampler->lens_samples_v;
     lens_jitter_v +=
         image_sampler->lens_min_v + (float_t)lens_v_index * image_sampler->lens_sample_width_v;
     *lens_sample_v = IMax(IMin(lens_jitter_v, image_sampler->lens_max_v),
@@ -349,10 +309,6 @@ GridImageSamplerAllocate(
     grid_image_sampler.lens_sample_width_v = (float_t)0.0;
     grid_image_sampler.jitter_pixel_samples = jitter_pixel_samples;
     grid_image_sampler.jitter_lens_samples = jitter_lens_samples;
-    grid_image_sampler.current_pixel_samples_u = 1;
-    grid_image_sampler.current_pixel_samples_v = 1;
-    grid_image_sampler.current_lens_samples_u = 1;
-    grid_image_sampler.current_lens_samples_v = 1;
 
     ISTATUS status = ImageSamplerAllocate(&grid_image_sampler_vtable,
                                           &grid_image_sampler,
